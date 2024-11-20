d2521ff1ea4e1b0e5e5f137eca208d3e0260373e
DenisWritesCode <deniswritescode@gmail.com>
2024-11-20 17:15:00 +0300

    Authentication works

---
+++ android/app/src/main/kotlin/com/example/mwaks_ai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mwaksai.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/auth_gate.dart
@@ -0,0 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              // For screens less than 800px in width.
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  //  child: Image.asset('assets/flutterfire_300x.png'),
+                  child: Text.rich(
+                    TextSpan(
+                      text: 'Mwaks AI',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                  ),
+                ),
+              );
+            }, // headerBuilder -> for Small Screens only.
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Welcome to MwaksAI, please sign in!')
+                    : const Text('Welcome to MwaksAI, please sign up!'),
+              );
+            },
+            footerBuilder: (context, action) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            sideBuilder: (context, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Text.rich(
+                    TextSpan(
+                      text: 'Mwaks AI',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        }
+
+        return const HomeScreen();
+      },
+    );
+  }
+}
