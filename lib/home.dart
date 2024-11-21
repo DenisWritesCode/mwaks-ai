530502747e5a25382b1ed796f4c0a912f7e72d8e
DenisWritesCode <deniswritescode@gmail.com>
2024-11-21 12:22:55 +0300

    001: Fixed app compiling problems

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,21 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  // File? image;
+  // Future pickImage() async {
+  //   try {
+  //     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+  //     if (image == null) return;
+  //     final imageTemp = File(image.path);
+  //     setState(() => this.image = imageTemp);
+  //   } on PlatformException catch (e) {
+  //     print('Failed to pick image: $e');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +65,19 @@ class HomeScreen extends StatelessWidget {
               'Mwaks AI!',
               style: Theme.of(context).textTheme.displaySmall,
             ),
+            MaterialButton(
+                color: Colors.blue,
+                child: const Text("Pick Image from Gallery",
+                    style: TextStyle(
+                        color: Colors.white70, fontWeight: FontWeight.bold)),
+                onPressed: () {}),
+            MaterialButton(
+                color: Colors.blue,
+                child: const Text("Pick Image from Camera",
+                    style: TextStyle(
+                        color: Colors.white70, fontWeight: FontWeight.bold)),
+                onPressed: () {}),
+            // const ImagePicker(key: ObjectKey('image_picker')),
             const SignOutButton(),
           ],
         ),
@@ -59,3 +85,46 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+/**
+ * 
+// class ImagePicker extends StatefulWidget {
+//   const ImagePicker({Key? key}) : super(key: key);
+
+//   @override
+//   _ImagePickerState createState() => _ImagePickerState();
+// }
+
+class _ImagePickerState extends State<ImagePicker> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example of Image Picker in flutter'),
+      ),
+      body: Center(
+        child: _image == null
+            ? const Text('No image selected.')
+            : Image.file(File(_image!.path)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: const Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+  Future getImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+}
+ */
+
